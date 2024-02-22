0b46d761aeef6f09add56746048a9b38185f4f37
Bikash Gurung <vikashtamu99@gmail.com>
2024-02-22 08:19:28 +0545

    Add change passowrd and get current user API

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,8 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -13,5 +15,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 export default router;
